9f00570015da86a34b4c6b44eaed97df97f37829
Steelo29 <94585075+Steelo29@users.noreply.github.com>
2023-04-03 19:53:27 -0400

    Add files via upload

---
+++ Newto.m
@@ -0,0 +1,19 @@
+clc;
+clear;
+x0 = input('Enter initial guess: ');
+n = input('Enter no of iterations: ');
+e = input('Enter tolerance: ');
+f = @(x)tanh(x);
+
+for i=1:n
+  xn = x0-(f(x0)./diff(f(x0)));
+  if abs(xn-x0)<e
+    fprintf('The approx root is: %f', x0)
+    break
+  else
+    x0 = xn;
+    xn = x0-(f(x0)./diff(f(x0)));
+  endif
+endfor
+ x0
+ xn
