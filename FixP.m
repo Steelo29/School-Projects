b27f79d2f834a6ef9846455b0d0a1f1bd820a155
Steelo29 <94585075+Steelo29@users.noreply.github.com>
2023-04-03 19:47:58 -0400

    Add files via upload

---
+++ FixP.m
@@ -0,0 +1,18 @@
+%Input Values
+g = input('Enter function: ');
+x0 = input('Enter initial guess: ');
+e = input('Enter tolerance: ');
+n = input('Enter no of iterations: ');
+
+for i = 1:n
+  x1 = g(x0);
+  fprintf('x%d %f\n',i, x1)
+  if abs(x1-x0)<e
+    break
+  endif
+  x0 = x1;
+end
+fprintf('The Root is: %f\n',x1)
+ fprintf('No of iterations: %d',i)
+
+
