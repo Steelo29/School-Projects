382bcef57d65ca8714a1c329b90e4efe71e16768
Steelo29 <94585075+Steelo29@users.noreply.github.com>
2023-04-03 19:51:36 -0400

    Add files via upload

---
+++ new_int.m
@@ -0,0 +1,13 @@
+x = input('Enter values for x: ');
+y = input('Enter values for y: ');
+p = input('Enter value of p: ');
+n = length(x);
+d = zeros(n,n);
+d(:,1) = y;
+for j = 2:n
+  for i = j:n
+    d(i,j) = (d(i,j-1) - d(i-1,j-1))/(x(i)-x(i-j+1));
+  endfor
+
+endfor
+d
