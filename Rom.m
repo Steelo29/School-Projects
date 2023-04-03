1ae7a6139c0a2da77a5c3069897e1f33b7a33dd2
Steelo29 <94585075+Steelo29@users.noreply.github.com>
2023-04-03 19:54:08 -0400

    Add files via upload

---
+++ Rom.m
@@ -0,0 +1,13 @@
+function [IR] = Rom(f,a,b,ni,lev)
+  for i = 1:(lev+1)
+    nsub = ni*2^(i-1)
+    h = (b-a)/nsub;
+    x = a:h:b;
+    F = f(x);
+    IR(i,1) = h*(F(nsub+1))/2 + h*sum(F(2:nsub));
+  endfor
+for j = 2:(lev+1)
+  for i = 1:(lev-j+2)
+    IR(i,j) = ((4^(j-1))*(IR(i+1, j-1))-(IR(i, j-1)))/((4^(j-1))-1);
+  endfor
+end
