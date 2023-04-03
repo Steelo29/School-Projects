832059b41db47797c45b4da46c712a674cc95558
Steelo29 <94585075+Steelo29@users.noreply.github.com>
2023-04-03 19:54:53 -0400

    Add files via upload

---
+++ Trap.m
@@ -0,0 +1,12 @@
+function [I] = Trap(f,a,b,n)
+format long
+h = (b-a)/n;
+x = a:h:b;
+L = length(x);
+F = zeros(1,L);
+for i = 1:L
+  F(i) = f(x(i));
+end
+I = h*(F(1)+F(n+1))/2 + h*sum(F(2:n))
+endfunction
+
