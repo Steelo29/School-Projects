9f4b25b7eafcf9edb6a8484f0328cf5dad075de5
Steelo29 <94585075+Steelo29@users.noreply.github.com>
2023-04-03 19:47:38 -0400

    Add files via upload

---
+++ FixedP.m
@@ -0,0 +1,16 @@
+clc;
+clear;
+format long;
+display('Function is chosen in y.m');
+xi = input('Enter initial guess: ');
+e = input('Enter tolerance: ');
+fxi = y(xi);
+
+while abs(fxi - xi) > e
+   if (fxi-xi) == 0
+     fprintf('The Root is: %14.14f\n', xi);
+   else
+    xi = fxi;
+   endif
+ endwhile
+ fprintf('The Root is: %f\n', xi);
