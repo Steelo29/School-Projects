a6b7fdbdae42863102f12b58bc577eb7533b4c04
Steelo29 <94585075+Steelo29@users.noreply.github.com>
2023-04-03 19:54:30 -0400

    Add files via upload

---
+++ Simps.m
@@ -0,0 +1,10 @@
+function [I] = Simps(f,a,b,n)
+  h = (b-a)/n;
+  x = a:h:b;
+  L = length(x);
+  F = zeros(1,L);
+  for i = 1:L
+    F(i) = f(i);
+  endfor
+I = (h/3)*(F(1)+4*(sum(F(2:2:n)))+2*(sum(F(3:2:n-1)))+F(n+1));
+endfunction
