4f109569e2566b2a0be66b49a074185b1e524f04
Steelo29 <94585075+Steelo29@users.noreply.github.com>
2023-04-03 19:53:10 -0400

    Add files via upload

---
+++ newt_inter.m
@@ -0,0 +1,24 @@
+function fp = newt_inter(x,y,p)
+
+ n = length(x);
+ a(1) = y(1);
+ b0 = y(1)
+ for k = 1:n-1
+   d(k,1) = (y(k+1)-y(k))/(x(k+1)-x(k));
+ endfor
+ for j = 2:n-1
+   for k = 1:n-j
+     d(k,j) = (d(k+1, j-1)-d(k,j-1))/(x(k+j)-x(k));
+   endfor
+ endfor
+ d
+ for j = 2:n
+   a(j)=d(1,j-1);
+ endfor
+Df(1) = 1;
+c(1)=a(1);
+ for j = 2:n
+   Df(j) = (p-x(j-1)).*Df(j-1);
+   c(j)=a(j).*Df(j);
+ endfor
+fp=sum(c);
