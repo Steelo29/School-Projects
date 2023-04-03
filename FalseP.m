a076539e8ba5fe267145ec7ae843ce516b07f253
Steelo29 <94585075+Steelo29@users.noreply.github.com>
2023-04-03 19:46:40 -0400

    Add files via upload

---
+++ FalseP.m
@@ -0,0 +1,35 @@
+clc;
+clear;
+format long;
+
+i=1;
+while(i)
+    a=input('Enter lower value:');
+    b=input('Enter upper value: ');
+    e=input('Enter tolerance: ');
+
+    if y(a)*y(b)<0
+        i=0;
+    else
+
+        warning('Incorrect interval');
+    end
+end
+if y(a)<0
+    xn=a;
+    xp=b;
+else
+    xn=b;
+    xp=a;
+end
+
+xm=a;
+while (abs(y(xm))>e)
+   xm=(xn*y(xp)-xp*y(xn))/(y(xp)-y(xn));
+    if y(xm)<0
+        xn=xm;
+    else
+        xp=xm;
+    end
+end
+fprintf('The Root is: %16.16f\n', xm)
