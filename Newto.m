d6a74f47a873562de7a19eda638155a1007248e4
Steelo29 <94585075+Steelo29@users.noreply.github.com>
2023-04-03 20:02:15 -0400

    Update Newto.m

--- Newto.m
+++ Newto.m
@@ -1,9 +1,10 @@
+%Alternative function to compute an approximation of a root using Newtons Method
 clc;
 clear;
 x0 = input('Enter initial guess: ');
 n = input('Enter no of iterations: ');
 e = input('Enter tolerance: ');
-f = @(x)tanh(x);
+f = @(x)tanh(x); %Example function
 
 for i=1:n
   xn = x0-(f(x0)./diff(f(x0)));
